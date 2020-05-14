88235c6aced9955e8b75d56f2982d64eb377ed2e
savmasse <sam.vanmassenhove@ugent.be>
2020-05-14 22:11:05 +0200

    Update authentication class

--- Authentication.py
+++ Authentication.py
@@ -5,15 +5,31 @@ class Authentication():
     # Standard values for the keys
     consumer_key = "H2oOuvgoFBQ4PA1K9Yd8CqdM6"        
     consumer_secret = "zSa6ulXVdNAl1Xk6TMSw48nVXIm88suBF06JzmT5XNSG2AIBxH"
-
-    def __init__(self, consumer_key=None, consumer_secret=None):
+    access_token = "1245776529768034306-YDUC9vTttvxvyhDhRVxGjfbt01p3bd"          
+    access_token_secret = "7cDHZiGpHSD2Y8Fe6RdRIpe75WephsSmfU6woDhHlD5BX"
+    
+    def __init__(self, 
+                 isApp=True,
+                 consumer_key=None, 
+                 consumer_secret=None, 
+                 access_token=None,
+                 access_token_secret=None):
 
         if consumer_key is None:
             consumer_key = Authentication.consumer_key
         if consumer_secret is None:
             consumer_secret = Authentication.consumer_secret
-
-        auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+        if access_token is None:
+            access_token = Authentication.access_token
+        if access_token_secret is None:
+            access_token_secret = Authentication.access_token_secret
+            
+        # App has higher allowed download rate, but cannot be used for streaming
+        if isApp:
+            auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+        else:
+            auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+            auth.set_access_token(access_token, access_token_secret)
 
         self.api = tweepy.API(
                         auth,
