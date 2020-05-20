641a40b7002fe1c186441424ebb7e60d3849fd82
savmasse <sam.vanmassenhove@ugent.be>
2020-05-20 20:09:39 +0200

    Add demo and classifier
    Add new improved classifier and a full demo of all functionality with streaming/IO/classification.

--- DenialPredictor.py
+++ DenialPredictor.py
@@ -1,24 +1,41 @@
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import roc_curve, auc, accuracy_score, f1_score, precision_score, recall_score
 
 class DenialPredictor():
     
-    def __init__(self, corpus, labels):
+    def __init__(self, corpus, labels, clf="nb"):
         self.corpus = corpus
         self.labels = labels
         self.vectorizer = CountVectorizer()
+        self.transformer = TfidfTransformer()
         
+        if clf == "nb":
+            self.classifier = MultinomialNB()
+        elif clf =="svm":
+            self.classifier = SGDClassifier(loss='log', penalty='l2',
+                                            alpha=1e-3, random_state=123,
+                                            max_iter=5, tol=None)
+        else:
+            raise "Invalid argument for classifier choice."
+            
     def fit_model(self, X_train, y_train):
-        self.model = MultinomialNB().fit(X_train, y_train)
+        p = Pipeline([
+                ('vect', self.vectorizer),
+                ('tfidf', self.transformer),
+                ('clf', self.classifier),
+        ])
+        self.model = p.fit(X_train, y_train)
     
     def _vectorize(self):
         return self.vectorizer.fit_transform(self.corpus)
     
     def train_test_split(self, split=0.3):
-        X = self._vectorize()
-        return train_test_split(X, self.labels, random_state=123, test_size=split)
+        #X = self._vectorize()
+        return train_test_split(self.corpus, self.labels, random_state=123, test_size=split)
     
     def predict(self, X):
         return self.model.predict(X)
---
+++ LivePredictionStream.py
@@ -0,0 +1,65 @@
+import tweepy
+import csv
+import re
+import string
+import numpy as np
+from PreProcessTweets import PreProcessTweets
+
+class LivePredictionStream(tweepy.StreamListener):
+
+    def __init__(self, 
+                 file_name, 
+                 predictor,
+                 num_iter=10, 
+                 verbose=0):
+        
+        super().__init__()
+        self.file_name = file_name
+        self.predictor = predictor
+        self.num_ter = num_iter
+        self.counter = 1
+        self.verbose = verbose
+
+    def on_status(self, status):
+                        
+        if not hasattr(status, "retweeted_status"):
+            
+            with open(self.file_name, "a", encoding='utf-8', newline='') as f:
+                                
+                text = status.extended_tweet["full_text"] if hasattr(status, "extended_tweet") else status.text
+                text = text.replace(",", "")
+                text = text.replace(";", "")
+                time = status.created_at
+                user = status.user.screen_name
+                                
+                # Preprocess
+                text = text.translate(str.maketrans('', '', string.punctuation))
+                text = re.sub(r'\s?http\S+', "", text)
+                text = " ".join(text.split())
+                
+                if self.verbose > 0:
+                    p = self.predictor.model.predict_proba([text])
+                    label = np.argmax(p)
+                    label_text = ("denial" if label == 0 else "normal")
+                    print(f"({self.counter}) {label_text} ({p[0][label]:.2f}): {text}")
+                    
+                location = status.user.location
+                coordinates = None
+                if status.place is not None:
+                    coordinates = status.place.bounding_box.coordinates
+
+                tags = PreProcessTweets.get_tags(text, True)
+                
+                writer = csv.writer(f, delimiter=";")
+                writer.writerow([text, tags, user, time, location, coordinates])
+                
+                # Increment counter
+                self.counter += 1
+                
+                # End streaming by raising exception
+                if self.counter > self.num_ter:
+                    raise Exception("Max iterations reached!")
+                
+    def on_error(self, status_code):
+        print("Error: ", status_code)
+        return False
